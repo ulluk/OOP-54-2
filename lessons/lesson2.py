d569fbabfe2049d32d56c26093635a4e553df223
ulluk <hofutop@gmail.com>
2025-05-09 17:51:47 +0600

    HW: Did homeWork_2

--- lessons/lesson2.py
+++ lessons/lesson2.py
@@ -1,29 +1,15 @@
-# Верблюжья нотация WarriorHero
-# Змеиная нотация   warrior_hero
-
-# Родительский класс
 class Hero:
     def __init__(self, name, lvl, hp):
         self.name = name
         self.lvl = lvl
         self.hp = hp
-
-    def introduce(self):
-        return print(f"Я {self.name}, мой уровень {self.lvl}")
-
     def action(self):
-        return print(f"{self.name} выполняет действие")
+        print(f"Базовое действие {self.name}")
 
+kirito = Hero("Kirito", 100, 1000)
+asuna = Hero("Asuna", 100 , 1000)
 
-# Дочерний класс
-class MageHero(Hero):
-    def __init__(self, name, lvl, hp, mp):
-        super().__init__(name, lvl, hp)
-        self.mp = mp
+kirito.action()
+asuna.action()
 
-    def cast_spell(self):
-        return print("Кастую огонь")
-    def action(self):
-        return print(f"{self.name} ничего не делает")
-mage_hero = MageHero("Маг",100,100,1000)
-mage_hero.action()
+print(asuna.hp)
